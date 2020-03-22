e01c2145597243a1686eabdaae1eff0e1fe75ca1
listentolife <605401951@qq.com>
2020-03-22 22:21:08 +0800

    feat: webpack配置

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vue-app-init
-一个手写webpack配置，基于vue架构的初始化app项目
+一个手写webpack配置，基于vue框架的初始化app项目
--- build/webpack.base.js
+++ build/webpack.base.js
@@ -3,12 +3,24 @@ const dev = require('./webpack.dev');
 const prod = require('./webpack.prod');
 // 引入webpack-merge，用于合并配置
 const merge = require('webpack-merge');
+// 引入path模块
+const path = require('path');
+
+let resolve = route => path.resolve(__dirname, route);
 
 medule.exports = (env) => {
   let isDev = env.development; // 获取环境参数
 
   const base = {
-    // ...
+    entry: { // 打包入口配置
+      main: resolve('../src/index.js') // main为指定打包入口名字
+    },
+    output: {
+      filename: isDev ? '[name].js' : '[name].js?v=[chunkhash]',
+      chunkFilename: isDev ? '[name].js' : '[name].js?v=[chunkhash]',
+      path: resolve('../dist'),
+      publicPath: '/'
+    }
   }
 
   if (isDev) {
